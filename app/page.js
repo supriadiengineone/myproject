35b8676e0e58a63be987a91212721e1b26841d05
supriadiibbr <148852395+supriadiibbr@users.noreply.github.com>
2024-12-10 15:18:37 +0700

    update fe input submit

--- app/page.js
+++ app/page.js
@@ -2,8 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-    Halo nama saya supriadi
-    </>
+      <div style={{
+        backgroundColor: '#ffff',
+        height: '100vh',
+        color: '#000'
+        }}>
+    <h1>
+      403 . Forbidden
+    </h1>
+      <p>
+        You do not have permission to access this resource.
+      </p>
+  </div>
   );
-}
+};
